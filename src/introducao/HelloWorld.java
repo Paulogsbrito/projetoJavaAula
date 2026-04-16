4edbd955e672931128daeac4b5c95f1cc72e1bd2
Paulo <paulogsbrito@gmail.com>
2026-04-16 17:01:17 -0300

    Criado novo Projeto

---
+++ src/introducao/HelloWorld.java
@@ -0,0 +1,13 @@
+package introducao;
+
+public class HelloWorld {
+	
+	public static void main(String [] args) {
+		// é o metodo principal que executa o código
+		
+		//Abaixo será a nossa lógica
+		
+		System.out.print("Hello World!!!!!"); // Comando de MSG identico ao Escreva do Portugol
+		
+	}
+}
---
+++ src/introducao/VariaveisConstantes.java
@@ -0,0 +1,29 @@
+package introducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		
+		//Variaveis:
+		
+		int idade = 27;
+		String nome = "Paulo Gustavo Brito";
+		float graus = 27.5f; // ponto flutuante 
+		boolean resposta; // lógico true/false
+		
+		//Constantes:
+		
+		//São valores imutaveis ou seja ao contrario de uma variavel que pode mudar, 
+		//e é uma boa pratica escrever com letra maiuscula 
+		
+		 final int QUANTIDADE = 50;
+		 final String TURMA = "Turma Java 84";
+		 final double AREA = 3.589;
+		 
+		 System.out.println(nome);
+		 System.out.println(QUANTIDADE);
+		 System.out.println(graus);
+
+	}
+
+}
